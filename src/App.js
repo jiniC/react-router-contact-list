25a49aeca53c25dbc64a48cdb392f8aeaa5ee247
jiniC <jinic323@gmail.com>
2018-07-22 11:39:18 +0900

    Creating and Contacting pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route,Link} from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -8,8 +9,28 @@ const App = () => (
 
 const Header = () => (
   <header>
-      <h1></h1>
+    <h1>HI JINI</h1>
+      <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contacts">Contact</Link></li>
+          </ul>
+      </nav>
+      <Route exact path="/welcome" component={Welcome} />
+      <Route exact path="/contacts" component={Contacts} />
   </header>
 )
+
+const Welcome = ({match}) => <h1>Welcome to the JINI APP</h1>
+
+// const Welcome = ({ match }) => console.log(match);
+
+const Contacts = () => (
+	<ul>
+		<li>Seojin</li>
+    <li>Dong</li>
+    <li>Choi man</li>
+	</ul>
+);
 
 export default App;
