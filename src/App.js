33f884cdc07cc990ee1e3e273deb4d179ca228ba
jiniC <jinic323@gmail.com>
2018-07-22 10:53:19 +0900

    Creating the Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => null;
+const App = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+const Header = () => (
+  <header>
+      <h1></h1>
+  </header>
+)
 
 export default App;
